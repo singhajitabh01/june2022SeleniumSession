d8ad88fab4197d9105919111b549ca2c54858d27
singhajitabh01 <singhajitabh01@gmail.com>
2022-10-23 12:25:33 +0530

    New Code add as per PR

--- src/main/java/seleniumSession/Ajitabh_NewFile.java
+++ src/main/java/seleniumSession/Ajitabh_NewFile.java
@@ -3,6 +3,7 @@ package seleniumSession;
 public class Ajitabh_NewFile {
 	public static void main(String[] args) {
 		System.out.println("Ajitabh Added new class for CART BRANCH");
+		System.out.println("NEw Code Added As per PR");
 	}
 
 }
